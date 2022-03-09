a492bdf2b4a482c740102cf9a00b63be6127a0a7
watcol <watcol@users.noreply.github.com>
2022-03-09 13:03:52 +0000

    deploy: d47d10db84771320ceac36fc543eb59f75d526e8

--- _nuxt/static/1646831010/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFrontendSample\u002F_nuxt\u002Fstatic\u002F1646831010",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002FFrontendSample\u002F",assetsPath:"\u002FFrontendSample\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fd8bbd47"}}}}(null));
---
+++ _nuxt/static/1646831031/404/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFrontendSample\u002F_nuxt\u002Fstatic\u002F1646831031",layout:"default",error:a,serverRendered:true,routePath:"\u002F404",config:{_app:{basePath:"\u002FFrontendSample\u002F",assetsPath:"\u002FFrontendSample\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"337d4091"}}}}(null));
